38015b60bc52e6ae22ac2abf4e5b7bb4e501870f
Mert Akdoğan2 <65607835+mertakd@users.noreply.github.com>
2023-04-30 22:00:15 +0300

    navigation back updated

--- app/src/main/java/com/example/todoappmvvm/MainActivity.kt
+++ app/src/main/java/com/example/todoappmvvm/MainActivity.kt
@@ -2,20 +2,29 @@ package com.example.todoappmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
+
         setupActionBarWithNavController(navController)
     }
 
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
 
 }
